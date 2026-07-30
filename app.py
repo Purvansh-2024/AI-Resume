bec5f9787dad5c681651872a03470d66549851b3
Purvansh-2024 <2006krishnachauhan2006@gmail.com>
2026-07-30 10:49:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ all_API = [TAVILY_API_KEY,GROQ_API_KEY,
 if not all(all_API):
     st.error("Must give API keys")
     st.stop()
-else all(all_API):
+elif all(all_API):
     st.success("API KEYS LOADED SUCCESSFULLY")
 else:
     st.info("PASS ALL API-KEYS")
