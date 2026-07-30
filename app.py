fda0fa5eba5fb56de295c98f6e67e7f524b5743f
Purvansh-2024 <2006krishnachauhan2006@gmail.com>
2026-07-30 10:30:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,6 +17,9 @@ st.title("AI RESUME GENERATOR")
 st.write("""This app helps user to build customized Professional Resume with latest Job apply links""")
 
 st.image("poster.png.jpeg")
+
+st.sidebat.title("Fill Important Details")
+st.sidebar.image("poster.png.jpeg")
 #============API KEYS====================
 # Step 3: API Keys
 TAVILY_API_KEY = "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"
