6773953a7ae2a15bee727f04bbe66bf3788df85e
Purvansh-2024 <2006krishnachauhan2006@gmail.com>
2026-07-31 09:49:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -35,9 +35,9 @@ elif all(all_API):
     st.success("API KEYS LOADED SUCCESSFULLY")
     #==================MODEL================
     model = ChatGoogleGenerativeAI(
-    model = 'gemini-3.5-flash-lite',
-    google_api_key = GOOGLE_API_KEY
-)
+               model = 'gemini-3.5-flash-lite',
+               google_api_key = GOOGLE_API_KEY
+    )
 else:
     st.info("PASS ALL API-KEYS")
 
