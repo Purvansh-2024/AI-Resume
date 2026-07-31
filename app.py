326f8e9b805e24c6a13ccdef5f8ff0151cf503af
Purvansh-2024 <2006krishnachauhan2006@gmail.com>
2026-07-31 09:23:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -33,6 +33,11 @@ if not all(all_API):
     st.stop()
 elif all(all_API):
     st.success("API KEYS LOADED SUCCESSFULLY")
+    #==================MODEL================
+    model = ChatGoogleGenerativeAI(
+    model = 'gemini-3.5-flash-lite',
+    google_api_key = GOOGLE_API_KEY
+)
 else:
     st.info("PASS ALL API-KEYS")
 
@@ -53,12 +58,6 @@ profile = st.sidebar.multiselect("Select Job Profile",
 st.markdown("""### GET USER INFO""")
 user_info = st.text_area("""Write you Resume Description: """)
 
-
-#============MODEL======================
-model = ChatGoogleGenerativeAI(
-    model = 'gemini-3.5-flash-lite',
-    google_api_key = GOOGLE_API_KEY
-)
 
 # response = model.invoke("Hello Buddy!")
 # response.content[-1]['text']
